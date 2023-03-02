b61a9cac4e241b5e6af0c948ac815aac44c86a74
unknown <shyue02@naver.com>
2023-03-02 13:48:01 +0900

    user-Entity 생성

--- src/main/java/site/metacoding/firstapp/domain/user/User.java
+++ src/main/java/site/metacoding/firstapp/domain/user/User.java
@@ -1,6 +1,18 @@
 package site.metacoding.firstapp.domain.user;
 
+import java.sql.Timestamp;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 
 public class User {
-
+    private Integer userId;
+    private String userName;
+    private String password;
+    private String email;
+    private String nickName;
+    private Timestamp createdAt;
 }
