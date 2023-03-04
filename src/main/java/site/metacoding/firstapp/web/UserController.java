8c55ac2841b69e45fd56457fab4bcb319579b794
unknown <shyue02@naver.com>
2023-03-04 21:15:12 +0900

    비밀번호 수정-수정

--- src/main/java/site/metacoding/firstapp/web/UserController.java
+++ src/main/java/site/metacoding/firstapp/web/UserController.java
@@ -34,6 +34,7 @@ public class UserController {
     
     @GetMapping("/user/passwordUpdateForm")
     public String 비밀번호수정() {
+    	System.out.println("비밀번호 수정!");
     	return "user/passwordUpdateForm";
     }
 
