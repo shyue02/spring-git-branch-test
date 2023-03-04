019e54872e6b693fc6b1140409eb1db1fbf4eb4e
unknown <shyue02@naver.com>
2023-03-04 21:22:13 +0900

    [충돌]

--- src/main/java/site/metacoding/firstapp/web/UserController.java
+++ src/main/java/site/metacoding/firstapp/web/UserController.java
@@ -34,6 +34,8 @@ public class UserController {
     
     @GetMapping("/user/passwordUpdateForm")
     public String 비밀번호수정() {
+    	System.out.println("비밀번호 수정!");
+    	System.out.println("비밀번호 수정2");
     	return "user/passwordUpdateForm";
     }
 
