d1af478c46ecbff44c541c6310889d0e8da99ffc
shyue02 <112357352+shyue02@users.noreply.github.com>
2023-03-04 20:36:19 +0900

    [merge]User - 비밀번호 체크 (#5)
    * 비밀번호 체크 생성
    
    * 비밀번호 체크 수정
    
    * 비밀번호 체크 수정2

--- src/main/java/site/metacoding/firstapp/web/UserController.java
+++ src/main/java/site/metacoding/firstapp/web/UserController.java
@@ -3,6 +3,8 @@ package site.metacoding.firstapp.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import ch.qos.logback.core.recovery.ResilientSyslogOutputStream;
+
 @Controller
 public class UserController {
 
@@ -17,4 +19,17 @@ public class UserController {
     	System.out.println("로그인 페이지 완료");
     	return "user/loginForm";
     }
+    
+    @GetMapping("/user/leaveCheckForm")
+    public String 회원탈퇴페이지() {
+    	return "user/leaveCheckForm";
+    }
+    
+    @GetMapping("/user/passwordCheckForm")
+    public String 비밀번호체크() {
+    	System.out.println("비밀번호 체크");
+    	System.out.println("'비밀번호 체크2");
+    	return "user/passwordCheckForm";
+    }
+
 }
