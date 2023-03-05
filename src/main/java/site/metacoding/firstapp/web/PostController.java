17bbec42409e51b784e5acfb9b6399e03aa867fa
unknown <shyue02@naver.com>
2023-03-05 14:29:25 +0900

    post 상세보기

--- src/main/java/site/metacoding/firstapp/web/PostController.java
+++ src/main/java/site/metacoding/firstapp/web/PostController.java
@@ -1,8 +1,14 @@
 package site.metacoding.firstapp.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PostController {
+	
+	@GetMapping("/post/detailForm")
+	public String 상세보기() {
+		return "post/detailForm";
+	}
 
 }
