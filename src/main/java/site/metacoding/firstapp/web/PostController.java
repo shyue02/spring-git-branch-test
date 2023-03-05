9314c1f184ef0df993d1769fe065bb6024455b39
shyue02 <112357352+shyue02@users.noreply.github.com>
2023-03-05 18:00:11 +0900

    Post (#15)
    * 게시글 쓰기

--- src/main/java/site/metacoding/firstapp/web/PostController.java
+++ src/main/java/site/metacoding/firstapp/web/PostController.java
@@ -3,6 +3,8 @@ package site.metacoding.firstapp.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.Delegate;
+
 @Controller
 public class PostController {
 	
@@ -10,6 +12,18 @@ public class PostController {
 	public String 목록보기() {
 		System.out.println("목록보기2");
 		return "category/listForm";
+	}
+
+	@GetMapping("/post/listForm")
+	public String 목록보기() {
+		System.out.println("listForm1");
+		return "post/listForm";
+	}
+	
+	@GetMapping("/post/writeForm")
+	public String 게시글쓰기() {
+		System.out.println("writeForm");
+		return "post/writeForm";
 	}
 
 }
