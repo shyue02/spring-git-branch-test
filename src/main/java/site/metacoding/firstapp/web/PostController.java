c35a2359ba9f307270da80e447e51c9c6d707993
unknown <shyue02@naver.com>
2023-03-06 01:12:01 +0900

    게시글 수정

--- src/main/java/site/metacoding/firstapp/web/PostController.java
+++ src/main/java/site/metacoding/firstapp/web/PostController.java
@@ -3,8 +3,6 @@ package site.metacoding.firstapp.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import lombok.Delegate;
-
 @Controller
 public class PostController {
 	
@@ -24,6 +22,11 @@ public class PostController {
 	public String 게시글쓰기() {
 		System.out.println("writeForm");
 		return "post/writeForm";
+	}
+	
+	@GetMapping("/post/updateForm")
+	public String 게시글수정() {
+		return "post/updateForm";
 	}
 
 }
