0c511ced98b329eb52ba8fe9e88f8a84e7b20c46
shyue02 <112357352+shyue02@users.noreply.github.com>
2023-03-05 14:31:25 +0900

    [merge]Post (#12)
    * post 상세보기

--- src/main/java/site/metacoding/firstapp/web/PostController.java
+++ src/main/java/site/metacoding/firstapp/web/PostController.java
@@ -1,8 +1,15 @@
 package site.metacoding.firstapp.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PostController {
+	
+	@GetMapping("/post/detailForm")
+	public String 상세보기() {
+		System.out.println("post detail");
+		return "post/detailForm";
+	}
 
 }
